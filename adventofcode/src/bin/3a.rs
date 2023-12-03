5596040d15d90999cb8b30fbbc5ba1cafe52fa39
zudo <44712315+zudo@users.noreply.github.com>
2023-12-03 09:23:03 +0100

    a

---
+++ adventofcode/src/bin/3a.rs
@@ -0,0 +1,42 @@
+use adventofcode::*;
+fn main() {
+    let input = read("input.txt");
+    let mut sum = 0;
+    for (i, line) in input.iter().enumerate() {
+        let mut n = 0;
+        let mut adjacent = false;
+        for (j, c) in line.chars().enumerate() {
+            if c.is_digit(10) {
+                n = format!("{}{}", n, c).parse::<u64>().unwrap();
+                for k in i.saturating_sub(1)..=i + 1 {
+                    if let Some(line) = input.get(k) {
+                        for l in j.saturating_sub(1)..=j + 1 {
+                            if let Some(c) = line.chars().nth(l) {
+                                // print!("{}", c);
+                                if c != '.' && !c.is_digit(10) {
+                                    adjacent = true;
+                                }
+                            }
+                        }
+                    }
+                    // println!()
+                }
+                if j == line.len() - 1 {
+                    if adjacent {
+                        sum += n;
+                        // println!("{}", n);
+                    }
+                }
+                // println!("{}", adjacent);
+            } else {
+                if adjacent {
+                    sum += n;
+                    // println!("{}", n);
+                }
+                n = 0;
+                adjacent = false;
+            }
+        }
+    }
+    println!("{}", sum);
+}
---
+++ adventofcode/src/bin/3b.rs
@@ -0,0 +1,30 @@
+use adventofcode::*;
+use regex::Regex;
+use std::collections::HashMap;
+fn main() {
+    let lines = read("input.txt");
+    let mut map: HashMap<(usize, usize), Vec<String>> = HashMap::new();
+    let re = Regex::new(r"\d+").unwrap();
+    for (row, line) in lines.iter().enumerate() {
+        for m in re.find_iter(line) {
+            let (start, end) = (m.start(), m.end());
+            let mut vec = vec![];
+            vec.push((row, start.saturating_sub(1)));
+            vec.push((row, end));
+            vec.extend((start.saturating_sub(1)..=end).map(|j| (row.saturating_sub(1), j)));
+            vec.extend((start.saturating_sub(1)..=end).map(|j| (row + 1, j)));
+            for (i, j) in vec {
+                if i < lines.len() && j < lines[i].len() && lines[i].chars().nth(j).unwrap() != '.'
+                {
+                    map.entry((i, j)).or_default().push(m.as_str().to_string());
+                }
+            }
+        }
+    }
+    let sum = map
+        .values()
+        .filter(|a| a.len() == 2)
+        .map(|a| a[0].parse::<i64>().unwrap() * a[1].parse::<i64>().unwrap())
+        .sum::<i64>();
+    println!("{}", sum);
+}
