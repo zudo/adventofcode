ce4dcfe33e80810c29ae453f8f5fa97090f05529
zudo <44712315+zudo@users.noreply.github.com>
2023-12-06 04:06:20 +0100

    b

--- src/bin/6a.rs
+++ src/bin/6a.rs
@@ -4,5 +4,8 @@ fn main() {
     let text = read("input.txt");
     let vec = text.split("\n").cv();
     let set = vec.cset();
+    let ii = vec.ii();
+    let ct: (&str, &str) = ii.ct().unwrap();
+    let ct2: (&str, &str) = ii.ct().unwrap();
     info!(?set);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
 pub use itertools::Itertools;
 pub use std::collections::*;
-use std::fmt::Display;
 use std::fs::File;
-use std::hash::Hash;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::io::Read;
@@ -44,66 +42,57 @@ pub fn read(path: impl AsRef<Path>) -> String {
     file.read_to_string(&mut buf).unwrap();
     buf
 }
-pub trait ExtraItertools: IntoIterator + Sized {
-    fn cset(self) -> HashSet<Self::Item>
+pub trait ExtraIntoIterator: IntoIterator + Sized {
+    fn ii(&self) -> Self::IntoIter
     where
-        Self::Item: Eq + Hash,
+        Self: Clone,
+    {
+        self.clone().into_iter()
+    }
+    fn cset(&self) -> HashSet<Self::Item>
+    where
+        Self::Item: Eq + std::hash::Hash,
+        Self: Clone,
     {
         self.ii().collect()
     }
-    fn cs(self) -> String
+    fn cstring(&self) -> String
     where
-        Self::Item: Display,
+        Self::Item: ToString,
+        Self: Clone,
     {
         self.ii().map(|x| x.to_string()).collect()
     }
-    fn cii(&self) -> std::vec::IntoIter<Self::Item>
-    where
-        Self: Clone,
-    {
-        self.clone().into_iter().collect_vec().into_iter()
-    }
-    fn ii(self) -> Self::IntoIter {
-        self.into_iter()
-    }
-    fn test(
-        self,
-        mut pass: impl FnMut(&Self::Item) -> bool,
-        mut fail: impl FnMut(&Self::Item) -> bool,
-    ) -> bool {
-        for item in self {
-            if pass(&item) {
-                return true;
-            }
-            if fail(&item) {
-                return false;
-            }
-        }
-        unreachable!("the iterator does not pass or fail");
-    }
-    fn one(&self) -> Self::Item
-    where
-        Self: Clone,
-    {
-        let mut iter = self.cii();
-        let item = iter.next().unwrap();
-        assert!(iter.next().is_none());
-        item
-    }
-    fn cv(self) -> Vec<Self::Item> {
-        self.into_iter().collect()
-    }
-    fn ca<const N: usize>(self) -> [Self::Item; N]
-    where
-        <Self as std::iter::IntoIterator>::Item: std::fmt::Debug,
-    {
-        self.cv().try_into().unwrap()
-    }
-    fn sumi(self) -> Self::Item
+    fn sumi(&self) -> Self::Item
     where
         <Self as std::iter::IntoIterator>::Item: std::ops::Add<Output = Self::Item> + Default,
+        Self: Clone,
     {
         self.ii().fold(Default::default(), |a, b| a + b)
     }
 }
-impl<T: IntoIterator + Sized + Clone> ExtraItertools for T {}
+impl<T: IntoIterator + Sized + Clone> ExtraIntoIterator for T {}
+pub trait ExtraIterator: Iterator + Sized {
+    fn cv(&self) -> Vec<Self::Item>
+    where
+        Self: Clone,
+    {
+        self.clone().collect_vec()
+    }
+    fn ca<const N: usize>(&self) -> [Self::Item; N]
+    where
+        <Self as std::iter::IntoIterator>::Item: std::fmt::Debug,
+        Self: Clone,
+    {
+        self.cv().try_into().unwrap()
+    }
+    fn ct<T>(&self) -> Option<T>
+    where
+        Self: Sized + Iterator<Item = T::Item>,
+        T: itertools::traits::HomogeneousTuple,
+        Self: Clone,
+    {
+        self.clone().collect_tuple()
+    }
+}
+impl<T: Iterator + Sized + Clone> ExtraIterator for T {}
