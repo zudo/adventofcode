45864cea2728b086c78c06649bade31a51896f70
zudo <44712315+zudo@users.noreply.github.com>
2023-12-06 05:42:36 +0100

    a

--- src/bin/6a.rs
+++ src/bin/6a.rs
@@ -2,13 +2,4 @@ use adventofcode::*;
 fn main() {
     tracing();
     let text = read("input.txt");
-    let vec = text.split("\n").cv();
-    let set_a = vec.cset();
-    let set_a = set(&vec);
-    let i: usize = int("123");
-    let s = str("asdf");
-    let ii = vec.ii();
-    let map = vec.cmap(|x| x.cs());
-    let m: usize = max([]);
-    info!(?set_a);
 }
