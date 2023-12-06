73115f6445abd568698e192a3ce6723c556855ca
zudo <44712315+zudo@users.noreply.github.com>
2023-12-06 08:10:47 +0100

    a

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ pub use tracing::warn;
 pub use tracing::*;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
+pub use unsafed::*;
 pub fn tracing() {
     tracing_subscriber::registry()
         .with(
@@ -43,114 +44,4 @@ pub fn read(path: impl AsRef<Path>) -> String {
     let mut buf = String::new();
     file.read_to_string(&mut buf).unwrap();
     buf
-}
-pub trait ExtraIntoIterator: IntoIterator {
-    fn ii(&self) -> Self::IntoIter
-    where
-        Self: Clone,
-    {
-        self.clone().into_iter()
-    }
-    fn cset(&self) -> HashSet<Self::Item>
-    where
-        Self::Item: Eq + std::hash::Hash,
-        Self: Clone,
-    {
-        self.ii().collect()
-    }
-    fn cmap<T, F>(&self, f: F) -> Vec<T>
-    where
-        F: FnMut(Self::Item) -> T,
-        Self: Clone,
-    {
-        self.ii().map(f).collect()
-    }
-    fn sums(&self) -> String
-    where
-        Self::Item: ToString,
-        Self: Clone,
-    {
-        self.ii().map(|x| x.to_string()).collect()
-    }
-    fn sumi(&self) -> Self::Item
-    where
-        <Self as std::iter::IntoIterator>::Item: std::ops::Add<Output = Self::Item> + Default,
-        Self: Clone,
-    {
-        self.ii().fold(Default::default(), |a, b| a + b)
-    }
-}
-impl<T: IntoIterator> ExtraIntoIterator for T {}
-pub trait ExtraIterator: Iterator {
-    fn cv(&self) -> Vec<Self::Item>
-    where
-        Self: Clone,
-    {
-        self.clone().collect_vec()
-    }
-    #[track_caller]
-    fn ca<const N: usize>(&self) -> [Self::Item; N]
-    where
-        <Self as std::iter::IntoIterator>::Item: std::fmt::Debug,
-        Self: Clone,
-    {
-        self.cv().try_into().unwrap()
-    }
-    fn ct<T>(&self) -> Option<T>
-    where
-        Self: Sized + Iterator<Item = T::Item>,
-        T: itertools::traits::HomogeneousTuple,
-        Self: Clone,
-    {
-        self.clone().collect_tuple()
-    }
-}
-impl<T: Iterator> ExtraIterator for T {}
-pub trait ExtraString: ToString {
-    fn cs(&self) -> String {
-        self.to_string()
-    }
-    #[track_caller]
-    fn ci<T: std::str::FromStr>(&self) -> T
-    where
-        <T as std::str::FromStr>::Err: std::fmt::Debug,
-    {
-        self.to_string().parse::<T>().unwrap()
-    }
-}
-impl<T: ToString> ExtraString for T {}
-#[track_caller]
-pub fn int<T: std::str::FromStr>(s: &str) -> T
-where
-    T::Err: std::fmt::Debug,
-{
-    s.parse::<T>().unwrap()
-}
-pub fn str(s: impl ToString) -> String {
-    s.to_string()
-}
-pub fn slice<T>(a: &[T], start: Option<usize>, stop: Option<usize>, step: usize) -> Vec<&T> {
-    let start = match start {
-        Some(start) => start,
-        None => 0,
-    };
-    let stop = match stop {
-        Some(stop) => stop,
-        None => a.len(),
-    };
-    a[start..stop].iter().step_by(step).collect()
-}
-pub fn len<T>(a: &[T]) -> usize {
-    a.len()
-}
-#[track_caller]
-pub fn max<T: Ord>(a: impl IntoIterator<Item = T>) -> T {
-    a.into_iter().max().unwrap()
-}
-#[track_caller]
-pub fn min<T: Ord>(a: impl IntoIterator<Item = T>) -> T {
-    a.into_iter().min().unwrap()
-}
-pub fn set<T: std::hash::Hash + Eq>(a: impl IntoIterator<Item = T>) -> HashSet<T> {
-    a.into_iter().collect()
 }
